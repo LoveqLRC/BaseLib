6025accdd6105b64d584a4f2d20ff5f8cb767e18
LoveqLRC <664215432@qq.com>
2018-01-12 14:25:36 +0800

    扩张BaseActivity

--- baselib/src/main/java/rc/loveq/baselib/ui/BaseActivity.java
+++ baselib/src/main/java/rc/loveq/baselib/ui/BaseActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import rc.loveq.baselib.R;
 
@@ -39,5 +40,25 @@ public abstract class BaseActivity extends AppCompatActivity {
             throw new NullPointerException();
         }
         setSupportActionBar(mToolbar);
+    }
+
+    protected void hideToolbarTitle() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        int menuId = getMenuId();
+        if (menuId == -1) {
+            return super.onCreateOptionsMenu(menu);
+        }
+        getMenuInflater().inflate(menuId, menu);
+        return true;
+    }
+
+    private int getMenuId() {
+        return -1;
     }
 }
